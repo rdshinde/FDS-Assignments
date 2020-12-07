86567bc9d969f87ca041d74620b8876823006f2c
RISHIKESH DNYANDEV SHINDE <67017632+rdshinde@users.noreply.github.com>
2020-12-07 12:13:54 +0530

    Update ASN_A_01.py

--- ASN_A_01.py
+++ ASN_A_01.py
@@ -1,55 +1,119 @@
 # Function for calculating intersection of two sets.
 def intersection(set1,set2):
-    intersect=[]
+    intersect= set()
     if len(set1)>=len(set2):
         for element in set1:
             if element in set2:
-                intersect.append(element)
+                intersect.add(element)
     else:
         for element in set2:
             if element in set1:
-                intersect.append(element)
+                intersect.add(element)
     return set(intersect)
 
-#print(intersection([2,4],[1,2,5]))
+#print(intersection([2,4,6],[1,2,5,6]))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 # Function for calculating either in set1 on in set2 but not in both.
 def either_of_two(set1,set2):
-    either=[]        
+    either=set()        
     for element in set1:
         if element not in set2:
-            either.append(element)
+            either.add(element)
     for element in set2:
         if element not in set1:
-            either.append(element)
+            either.add(element)
     return set(either)
 
 #print(either_of_two([1,2,3,4],[1,2,6,7,8,9]))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 # Funcion to calculate elements in neither of two sets but in universal sets.
 def neither_of_two(set1,set2,universal_set):
     #universal_set=[1,2,3,4,5,6,7,8,9]
     union = set1 + set2
-    neither=[]
+    neither=set()
     for element in universal_set:
         if element not in union:
-            neither.append(element)
+            neither.add(element)
     return(set(neither))
 
-#print(neither_of_two([1,2,3],[2,3,4,5,6]))
+#print(neither_of_two([1,2,3],[2,3,4,5,6],[1,2,3,4,5,6,7,8,9]))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 # Function to calculate elements in set1 and set2 but not in set3.
 def not_in_third(set1,set2,set3):
-    not_third = []
+    not_third = set()
     intersect_A_B = intersection(set1,set2)
     for element in intersect_A_B:
         if element not in set3:
-            not_third.append(element)
+            not_third.add(element)
     return set(not_third)
  
-
 #print(not_in_third([1,2,3,12,13,14,45],[3,4,12,13,14,5,6],[2,4,6,7,8,9,0]))            
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+'''
 universal_set = list(map(int,input('Enter roll numbers of students in class seperated with space: ').split()))
 cricket = list(map(int,input('Enter roll numbers of students who plays cricket seperated with space: ').split()))
 badminton = list(map(int,input('Enter roll numbers of students who plays badminton seperated with space: ').split()))
@@ -65,4 +129,4 @@ cricket_nor_badmint = neither_of_two(cricket,badminton,universal_set)
 print('Students who play neither cricket nor badminton: ',cricket_nor_badmint)
 
 crick_football_not_badmint = not_in_third(cricket,football,badminton)
-print('Stuudent who play cricket and football but not badminton: ',crick_football_not_badmint) 
+print('Stuudent who play cricket and football but not badminton: ',crick_football_not_badmint) '''
